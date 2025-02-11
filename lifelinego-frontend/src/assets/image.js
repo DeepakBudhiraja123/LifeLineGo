d1b712eefeab57ca8f138550d0713d4792dd81a9
Deepak <budhirajadeepak123@gmail.com>
2025-02-11 14:12:36 +0530

    Sigin Completed

--- lifelinego-frontend/src/assets/image.js
+++ lifelinego-frontend/src/assets/image.js
@@ -1,7 +1,11 @@
 import Loading from "./loading.webp"
+import Signin from "./Signin.png";
+import Signup from "./Signup.png";
 
 const images = {
-    Loading
+    Loading,
+    Signin,
+    Signup
 }
 
 export default images;
--- lifelinego-frontend/src/pages/Signin/Signin.jsx
+++ lifelinego-frontend/src/pages/Signin/Signin.jsx
@@ -1,12 +1,39 @@
-import React from 'react'
-import "./Signin.css"
+import React, { useState } from "react";
+import "./Signin.css";
+import images from "../../assets/image";
 
 const Signin = () => {
-  return (
-    <div>
-      here
-    </div>
-  )
-}
+  const [isSignUp, setIsSignUp] = useState(false);
 
-export default Signin
+  return (
+    <div className={`auth-container ${isSignUp ? "signup-mode" : "signin-mode"}`}>
+      <div className="auth-left">
+        <img
+          src={isSignUp ? images.Signup : images.Signin}
+          alt={isSignUp ? "Sign Up" : "Sign In"}
+        />
+      </div>
+      
+      {/* Right Section */}
+      <div className="auth-right">
+        <div className="auth-box">
+          <h2>{isSignUp ? "Create an Account" : "Welcome Back"}</h2>
+          <form>
+            {isSignUp && <input type="text" placeholder="Full Name" required />}
+            <input type="email" placeholder="Email" required />
+            <input type="password" placeholder="Password" required />
+            <button type="submit">{isSignUp ? "Sign Up" : "Sign In"}</button>
+          </form>
+          <p>
+            {isSignUp ? "Already have an account? " : "Don't have an account? "}
+            <span onClick={() => setIsSignUp(!isSignUp)}>
+              {isSignUp ? "Sign In" : "Sign Up"}
+            </span>
+          </p>
+        </div>
+      </div>
+    </div>
+  );
+};
+
+export default Signin;
